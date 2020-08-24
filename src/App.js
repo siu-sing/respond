945c9533b7820a00c744c904fec544e9bb786445
siu-sing <lai.siusing@gmail.com>
2020-08-24 23:05:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Row, Container, Col } from 'react-bootstrap';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let playYes = () => {
+        console.log("yes")
+        const audioEl = document.getElementsByClassName("audio-yes")[0]
+        audioEl.play()
+    }
+
+    let playNo = () => {
+        console.log("no")
+        const audioEl = document.getElementsByClassName("audio-no")[0]
+        audioEl.play()
+    }
+
+    return (
+        <Container className="">
+            <Row className="">
+                <Col className="yes text-center d-flex m-2">
+                    <Button onClick={playYes} className="btn-block btn-success">
+                        <span className="display-1">YES</span>
+                        <audio className="audio-yes">
+                            <source src="https://www.pacdv.com/sounds/voices/yes-5.wav"></source>
+                        </audio>
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="no text-center d-flex m-2">
+                    <Button onClick={playNo} className="btn-block btn-danger">
+                        <span className="display-1">NO</span>
+                        <audio className="audio-no">
+                            <source src="https://www.pacdv.com/sounds/voices/no-2.wav"></source>
+                        </audio>
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
